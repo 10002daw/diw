585838b840a8d1a1c8ec07f803459b72eb8e9459
Eduardo Jiménez Bahíllo <10002@iespuertasdelcampo.es>
2020-11-13 14:29:51 +0100

    2020-11-12 Clase

--- PracticaHTML/form.php
+++ PracticaHTML/form.php
@@ -12,6 +12,40 @@
                 echo $key.": ".$value."<br>";
             }
         }
+
+        if ( $_POST ) {
+    ?>
+    <table border="1">
+        <tr>
+            <th colspan="2">Personal information</th>
+        </tr>
+        <tr>
+            <td>Title</td>
+            <td><?=$_POST[title]?></td>
+        </tr>
+        <tr>
+            <td>First name</td>
+            <td><?=$_POST[first_name]?></td>
+        </tr>
+        <tr>
+            <td>Last name</td>
+            <td><?=$_POST[last_name]?></td>
+        </tr>
+        <tr>
+            <td>Birthdate</td>
+            <td><?=$_POST[birthdate]?></td>
+        </tr>
+        <tr>
+            <td>City</td>
+            <td><?=$_POST[city]?></td>
+        </tr>
+        <tr>
+            <td>Postcode</td>
+            <td><?=$_POST[postcode]?></td>
+        </tr>
+    </table>
+    <?php
+        }
     ?>
 </body>
 </html>
